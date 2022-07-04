cd7dd99d7019e2abcffe21d02414e06c01da52c8
Pito Peter <piotr.wozniak.2991@gmail.com>
2022-07-04 20:26:35 +0700

    Initial Commit

---
+++ MovieChecker/MovieCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCheckerApp.swift
+//  MovieChecker
+//
+//  Created by Piotr Woźniak on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
