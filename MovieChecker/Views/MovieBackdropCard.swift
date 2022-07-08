11cbb23226799248a509cd1e316b9913430a4b41
Pito Peter <piotr.wozniak.2991@gmail.com>
2022-07-08 20:21:17 +0700

    adding ObservableObject

---
+++ MovieChecker/Views/MovieBackdropCard.swift
@@ -0,0 +1,20 @@
+//
+//  MovieBackdropCard.swift
+//  MovieChecker
+//
+//  Created by Piotr Woźniak on 08/07/2022.
+//
+
+import SwiftUI
+
+struct MovieBackdropCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieBackdropCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieBackdropCard()
+    }
+}
