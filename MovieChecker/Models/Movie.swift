f7225ffd003da64337a1235f27502d86456224ed
Pito Peter <piotr.wozniak.2991@gmail.com>
2022-07-15 19:23:50 +0700

    detail view

--- MovieChecker/Models/Movie.swift
+++ MovieChecker/Models/Movie.swift
@@ -21,10 +21,90 @@ struct Movie: Decodable, Identifiable {
     let voteCount: Int
     let runtime: Int?
     
+    let releaseDate: String?
+    let genres: [MovieGenre]?
+    let credits: MovieCredit?
+    
+    static private let yearFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy"
+        return formatter
+    }()
+    
+    static private let durationFormatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .full
+        formatter.allowedUnits = [.hour, .minute]
+        return formatter
+    }()
+    
     var backdropURL: URL {
         return URL(string: "https://image.tmdb.org/t/p/w500\(backdropPath ?? "")")!
     }
     var posterURL: URL {
         return URL(string: "https://image.tmdb.org/t/p/w500\(posterPath ?? "")")!
     }
+    var genreText: String {
+        genres?.first?.name ?? "n/a"
+    }
+    var ratingText: String {
+        let rating = Int(voteAverage)
+        let ratingText = (0..<rating).reduce("") { (acc, _) -> String in
+            return acc + "★"
+        }
+        return ratingText
+    }
+    var scoreText: String {
+        guard ratingText.count > 0 else {
+            return "n/a"
+        }
+        return "\(ratingText.count)/10"
+    }
+    var yearText: String {
+        guard let releaseDate = releaseDate, let date = Utilis.dateFormatter.date(from: releaseDate) else {
+            return "n/a"
+        }
+        return Movie.yearFormatter.string(from: date)
+    }
+    var durationText: String {
+        guard let runtime = runtime, runtime > 0 else {
+            return "n/a"
+        }
+        return Movie.durationFormatter.string(from: TimeInterval(runtime) * 60) ?? "n/a"
+    }
+    var cast: [MovieCast]? {
+        credits?.cast
+    }
+    var crew: [MovieCrew]? {
+        credits?.crew
+    }
+    var directors: [MovieCrew]? {
+        crew?.filter { $0.job.lowercased() == "director" }
+    }
+    var producers: [MovieCrew]? {
+        crew?.filter { $0.job.lowercased() == "producer" }
+    }
+    var screenWriters: [MovieCrew]? {
+        crew?.filter { $0.job.lowercased() == "story" }
+    }
+}
+
+struct MovieGenre: Decodable {
+    let name: String
+}
+
+struct MovieCredit: Decodable {
+    let cast: [MovieCast]
+    let crew: [MovieCrew]
+}
+
+struct MovieCast: Decodable, Identifiable {
+    let id: Int
+    let character: String
+    let name: String
+}
+struct MovieCrew: Decodable, Identifiable {
+    let id: Int
+    let job: String
+    let name: String
 }
